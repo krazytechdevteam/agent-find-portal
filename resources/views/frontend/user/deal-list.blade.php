877a1128e16b8415a66b7a358f6b94db2d3f8c1d
Sanjay Kumar Das <dassanjay0130@gmail.com>
2020-03-28 14:53:11 +0530

    CSS Moved to respective view file

--- resources/views/frontend/user/deal-list.blade.php
+++ resources/views/frontend/user/deal-list.blade.php
@@ -6,6 +6,58 @@
          
 @section('content')
 
+<style type="text/css">
+  .bold-text {
+      margin: 0px;
+      font-size: 20px;
+      color: #e7161b;
+      font-weight: 500;
+  }
+  .bold-text a {
+    color: #e7161b !important
+  }
+  .light-text {
+
+    margin: 0px;
+    font-size: 12px;
+    font-weight: 500;
+    color: #9e9e9e;
+  }
+
+  .table tbody tr td {
+    
+    font-size: 1em;
+    border: none;
+    padding: 18px 10px;
+  }
+  
+  .table thead tr, .table tbody tr {
+    border-bottom: 1px solid #ccc !important
+  }
+  .btn-default {
+    background-color: #fff;
+    color: #4cae4c;
+    border-color: #4cae4c;
+  }
+
+  .btn {
+    border-width: 2px;
+  }
+  .table thead tr th {
+    /* color: #b11a1e; */
+    /* text-transform: uppercase; */
+    font-size: 1em;
+    font-weight: 500;
+    font-size: 15px;
+    /* font-family: "proxima nova semibold"; */
+    border-bottom: -1px;
+    color: #333;
+    padding: 11px 7px;
+  }
+  label {
+    font-weight: 500 !important
+  }
+</style>
 
      <div class="dashboard-header m-b-30">
       <div class="container">
@@ -44,7 +96,11 @@
               
                           <tr>
                             <td>
-                                <p class="bold-text"><?php echo $r->DEAL_NAME; ?></p>
+                                <p class="bold-text">
+                                  <a href="<?php echo url('/')  . '/deal-details/' . $r->DEAL_ID; ?>">
+                                      <?php echo $r->DEAL_NAME; ?>
+                                  </a>
+                                </p>
                                 <p class="light-text"><?php echo $r->DEAL_EMAIL; ?></p>
                                 <p class="light-text"><?php echo $r->DEAL_PHONE; ?></p>
 							</td>
