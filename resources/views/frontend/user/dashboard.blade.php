245a1832a77f2934e8e32b4024554ac6f12e40f4
Ashim Kumar Sahoo <ashimkumar.sahoo@gmail.com>
2020-04-09 13:06:49 +0530

    issue fixes

--- resources/views/frontend/user/dashboard.blade.php
+++ resources/views/frontend/user/dashboard.blade.php
@@ -273,53 +273,6 @@
 <script>
 window.addEventListener("load", function(){
 
-  am4core.ready(function() {
-
-    // Themes begin
-    am4core.useTheme(am4themes_animated);
-    // Themes end
-
-    // create chart
-    var chart = am4core.create("deals_actively_looking", am4charts.GaugeChart);
-    chart.innerRadius = -15;
-
-    var axis = chart.xAxes.push(new am4charts.ValueAxis());
-    axis.min = 0;
-    axis.max = 100;
-    axis.strictMinMax = true;
-
-    var colorSet = new am4core.ColorSet();
-
-    var gradient = new am4core.LinearGradient();
-    gradient.stops.push({color:am4core.color("red")})
-    gradient.stops.push({color:am4core.color("yellow")})
-    gradient.stops.push({color:am4core.color("green")})
-
-    axis.renderer.line.stroke = gradient;
-    axis.renderer.line.strokeWidth = 15;
-    axis.renderer.line.strokeOpacity = 1;
-
-    axis.renderer.grid.template.disabled = true;
-
-    var hand = chart.hands.push(new am4charts.ClockHand());
-    hand.radius = am4core.percent(97);
-
-    setTimeout(function() {
-
-        var val = document.getElementById('deals_active').value;
-        val = parseInt(val);
-
-        if(val > 100) {
-          
-          axis.max = val + 30;
-        }
-
-        hand.showValue(val, 1000, am4core.ease.cubicOut);
-    }, 2000);
-
-
-  }); // end am4core.ready()
-
   // Themes begin
 am4core.useTheme(am4themes_animated);
 
